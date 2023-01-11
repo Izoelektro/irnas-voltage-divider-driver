fd5aa9dcb187da06f39d6836f808874afad4303e
MarkoSagadin <marko.sagadin42@gmail.com>
2023-01-11 13:37:52 +0100

    Change dts interface, adjust and refactor voltage divider
    All changes in the files started with changes in
    dts/bindings/irnas,voltage-divider.yaml.
    The way how resistor values are specified changed, additional property
    for measuring internal vdd was added.
    Other changes:
    - In drivers source files the devicetree macros had to be adjusted due to
    that (they had to change anyway as the used label properties, which were
    removed in the ncs 2.2.0).
    - The rest of the changes in the driver source files are related
    to validation of device tree settings and general cleanup.
    - Header includes in all files were updated to include zephyr path.
    - Driver is now automatically compiled with the application, if a node
      with 'compatible' property is set to "irnas,voltage-divider".

--- samples/basic/src/main.c
+++ samples/basic/src/main.c
@@ -1,38 +1,27 @@
-
-#include <device.h>
-#include <devicetree.h>
-#include <logging/log.h>
 #include <voltage_divider.h>
-#include <zephyr.h>
+
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main);
 
-#define VOLTAGE_DIVIDER_DEV "VBAT"
+const struct device *dev = DEVICE_DT_GET(DT_PATH(vbatt));
 
 void main(void)
 {
 	LOG_INF("Voltage divider test running on: %s", CONFIG_BOARD);
 
-	const struct device *dev;
 	int err;
 
-	// get device binding
-	dev = device_get_binding(VOLTAGE_DIVIDER_DEV);
-	if (dev == NULL) {
-		LOG_ERR("Device unknown (%s)", VOLTAGE_DIVIDER_DEV);
-		return;
-	}
-
-	// take a sample
 	while (1) {
-
 		err = voltage_divider_sample(dev);
 		if (err < 0) {
 			LOG_ERR("voltage_divider_sample, err: %d", err);
 		} else {
-			LOG_INF("Voltage divider sample valeue: %d", err);
+			LOG_INF("Voltage divider sample value: %d", err);
 		}
 
-		k_sleep(K_SECONDS(3));
+		k_sleep(K_SECONDS(1));
 	}
 }
